7d37fb6289799927757019c08fbb49505713b449
VarFer <scheleimayer@gmail.com>
2024-08-29 12:12:41 -0400

    update

---
+++ eclipse-workspace/DibujandoPuntos/src/dibujapuntos/SoloPuntos.java
@@ -0,0 +1,18 @@
+package dibujapuntos;
+
+import java.util.Scanner;
+
+public class SoloPuntos {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+				int n = sc.nextInt();
+				for(int i=0;i<n;i++) {
+				System.out.printf("*");
+				}
+				System.out.printf("\n");
+		
+	}
+
+}
